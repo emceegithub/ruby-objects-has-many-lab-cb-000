eb295b008c7a3c328499504d1114699f77700b76
Mark Cruz <markchriscruz@yahoo.com>
2018-03-13 04:12:33 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,4 @@
 class Artist
-  
   attr_accessor :name
   
   def initialize(name)
@@ -11,8 +10,8 @@ class Artist
     return @songs
   end
   
-  def add_song
-    
+  def add_song(song)
+    @songs << song
+    song.artist = self
   end
-
 end
